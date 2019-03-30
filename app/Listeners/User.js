bec058a419de426ec0e604d4e5db697f49d0cbe0
michael <zdong2010@gmail.com>
2019-03-30 19:57:38 +0800

    event to handle and listener

---
+++ app/Listeners/User.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const Ws = use('Ws')
+
+const User = exports = module.exports = {}
+
+User.method = async () => {
+}
+
+User.log = async (user) => {
+  console.log('user.login: %s just logged in.', user.username)
+
+  Ws.getChannel('demo').topic('demo').broadcast('message', {
+    username: user.username,
+    content: 'just logged in.'
+  })
+}
